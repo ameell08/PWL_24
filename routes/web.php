e7a7ee4d10c534e83b820364f42f4df6de15fff9
ameell08 <amelia.safitri.wahyu@gmail.com>
2024-09-04 14:48:04 +0700

    Jobsheet2-praktikum1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// --- PRAKTIKUM 1 ---
+//BASIC ROUTING
+// no. 02
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+// no. 04
+Route::get('/world', function () {
+    return 'world';
+});
+
+// no. 06
+Route::get('/greeter', function () {
+    return 'Selamat Datang';
+});
+
+//no.07
+Route::get('/about', function () {
+    return '2241760008 | Amelia Wahyu Safitri';
+});
+
+
+//ROUTE PARAMETERS
+// no.08
+Route::get('/user/{name}', function ($name) {
+    return 'Nama Saya  '.$name;
+});
+
+// no. 11
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke- '.$postId. " komentar ke- : ".$commentId;
+});
+
+// no.13
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' .$id;
+});
+
+//OPTIONAL PARAMETERS
+// no. 14
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama Saya  ' .$name;
+});
+
+// no. 17
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama Saya  ' .$name;
+});
+
